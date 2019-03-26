9d8f5159bb5b375a3f0416d71f9511e45d904c0d
Kai Yan <u3514123@connect.hku.hk>
2019-03-26 10:16:39 +0800

    update avg_mktret

--- avg_mktret.py
+++ avg_mktret.py
@@ -3,42 +3,45 @@ from sas7bdat import SAS7BDAT
 import csv
 
 NAME = "medianret"
-
 IN_FILE = NAME + ".sas7bdat"
 OUT_FILE = NAME + ".csv"
 
-with SAS7BDAT(IN_FILE) as f:
-	raw = [row for row in f][1:]
-
 data = []
-country = ""
-start = False
-for obs in raw:
-	if obs[0] != country:
-		start = False
-		country = obs[0]
-	if obs[-1] != None:
-		start = True
-	if start:
-		data.append(obs)
-	
 
 
-for i in range(len(data)):
-	obs = data[i]
-	if obs[-1] == None:
-		pre = [x[-1] for x in data[:i] if x[0] == obs[0]]
-		obs[-1] = sum(pre)/len(pre)
+def main():
+
+	with SAS7BDAT(IN_FILE) as f:
+		raw = [row for row in f][1:]
+
+	country = ""
+	start = False
+	for obs in raw:
+		if obs[0] != country:
+			start = False
+			country = obs[0]
+		if obs[-1] != None:
+			start = True
+		if start:
+			data.append(obs)
+		
+
+	for i in range(len(data)):
+		obs = data[i]
+		if obs[-1] == None:
+			pre = [x[-1] for x in data[:i] if x[0] == obs[0]]
+			obs[-1] = sum(pre)/len(pre)
 
 
-
-with open(OUT_FILE, 'w', newline='') as csvfile:
-	writer = csv.writer(csvfile)
-	writer.writerow(['country', 'year', 'ret', 'ret2'])
-	for obs in data:
-		writer.writerow(obs)
-
+	with open(OUT_FILE, 'w', newline='') as csvfile:
+		writer = csv.writer(csvfile)
+		writer.writerow(['country', 'year', 'ret', 'ret2'])
+		for obs in data:
+			writer.writerow(obs)
 
 
-print(len(data))
-print(data[:8])
+if __name__ == '__main__':
+	main()
+
+	print(len(data))
+	print(data[:8])
